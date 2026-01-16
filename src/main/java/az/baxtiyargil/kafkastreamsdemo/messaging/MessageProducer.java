f7b3ffa0cd9f1fb4534f79e3627ca9a3a889b91d
Bakhtiyar V. Karimov <bext97@mail.ru>
2026-01-16 12:06:45 +0400

    retry producer correction

--- src/main/java/az/baxtiyargil/kafkastreamsdemo/messaging/MessageProducer.java
+++ src/main/java/az/baxtiyargil/kafkastreamsdemo/messaging/MessageProducer.java
@@ -2,6 +2,7 @@ package az.baxtiyargil.kafkastreamsdemo.messaging;
 
 import az.baxtiyargil.kafkastreamsdemo.configuration.properties.ApplicationConstants.Messaging;
 import az.baxtiyargil.kafkastreamsdemo.messaging.event.DomainEvent;
+import az.baxtiyargil.kafkastreamsdemo.messaging.event.EventType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.function.StreamBridge;
@@ -26,15 +27,20 @@ public class MessageProducer {
         streamBridge.send(Messaging.OutputChannel.ORDER, message);
     }
 
-    public <T extends DomainEvent> void sendOrderRetryEvent(Message<?> message, Throwable throwable) {
+    public <T extends DomainEvent> void sendRetryEvent(Message<?> message, Throwable throwable) {
         int retryCount = (Integer) message.getHeaders().getOrDefault(Messaging.HEADER_X_RETRY_COUNT, 0);
         var messageKey = message.getHeaders().getOrDefault(KafkaHeaders.RECEIVED_KEY, "");
 
+        String outputChannel = null;
+        if (message.getPayload() instanceof DomainEvent event) {
+            var eventType = EventType.of(event.getType());
+            outputChannel = eventType.getOutputChannelName();
+        }
         Message<?> retryMessage = MessageBuilder.fromMessage(message)
                 .setHeader(Messaging.HEADER_X_RETRY_COUNT, ++retryCount)
                 .setHeader(Messaging.HEADER_X_RETRY_REASON, throwable.getClass().getSimpleName())
                 .setHeader(KafkaHeaders.KEY, messageKey)
                 .build();
-        streamBridge.send(Messaging.OutputChannel.ORDER_RETRY, retryMessage);
+        streamBridge.send(outputChannel, retryMessage);
     }
 }
